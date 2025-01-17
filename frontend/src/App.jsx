d93091c0275ffb45fd10b2e7cb6305c58eb81965
Sahil Sharma <vlsharma713@gmail.com>
2025-01-17 23:19:32 +0530

    Add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React </h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
