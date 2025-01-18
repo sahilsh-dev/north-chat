59194232ffc2e3250b14b685cf44799663386368
Sahil Sharma <vlsharma713@gmail.com>
2025-01-18 18:50:42 +0530

    Add dark theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
+import { ThemeProvider } from "./components/ThemeProvider";
 import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1>Vite + React </h1>
       <Button>Click me</Button>
-    </>
+    </ThemeProvider>
   );
 }
 
