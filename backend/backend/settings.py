f5589dd17bf4be1aa01f0651a00e5d71c3acc7a4
Sahil Sharma <vlsharma713@gmail.com>
2025-03-18 22:44:11 +0530

    Update debug env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
